a9c58375228762538f21f8f893807a97701d42f5
Gavin <167050397+imhubgitting@users.noreply.github.com>
2025-02-12 15:25:17 -0500

    Main Draws Blank Screen

--- src/main.py
+++ src/main.py
@@ -1,1 +1,21 @@
 # Game Loop
+
+import pygame
+
+pygame.init()
+
+WINDOW_SIZE = (600,600)
+screen = pygame.display.set_mode(WINDOW_SIZE)
+
+def draw(screen):
+    screen.fill('white')
+    pygame.display.update()
+    
+if __name__ == '__main__':
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        draw(screen)
+        

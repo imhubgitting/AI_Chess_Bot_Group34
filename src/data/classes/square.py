ea76209b20122aee60cfbf062c3ff1ef9e5d6353
Gavin <167050397+imhubgitting@users.noreply.github.com>
2025-02-16 18:36:09 -0500

    Update all class files to have TODO functions and comments for every main function

---
+++ src/data/classes/square.py
@@ -0,0 +1,18 @@
+# Square Initialization
+import pygame as py
+
+# TODO Class Square:
+    # * Setup Functions
+        # TODO def __init__(square, x, y, width, height):
+            # Initializes chessboard tile with (x, y), width/height
+            
+
+    # * Helper Functions
+        # TODO get_coord(self):
+            # Converts (x, y) to chess notation (e.g. a1, h8, ...) (a-h, 1-8)
+
+
+    # * Rendering Functions
+        # TODO def draw(self, display):
+            # Draws square with appropriate color, highlighting
+            # When a piece is on square, centers and displays piece's image

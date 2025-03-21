c70688bfda21bff7ef184020d8cae5dcfb3d6b23
Julian Jordan <jaj21h@fsu.edu>
2025-03-21 13:02:09 -0400

    bishop logic

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # AI_Chess_Bot_Group34
+
+run to clean repo after testing:
+rm -r src/data/__pycache__
+rm -r src/data/classes/__pycache__
+rm -r src/data/classes/pieces/__pycache__
--- src/data/classes/pieces/bishop.py
+++ src/data/classes/pieces/bishop.py
@@ -1,10 +1,34 @@
 # Bishop Logic
 
 import pygame as py
-# TODO from data.classes.piece import Piece
+from data.classes.piece import Piece
 
-# TODO class Bishop(Piece):
-    # TODO def __init__(self, pos, color, board):
+class Bishop(Piece):
+    def __init__(self, pos, color, board):
+        super().__init__(pos, color, board)
+        img_path = 'src/data/classes/imgs_pieces/' + color[0] + '_bishop'
+        self.img = py.image.load(img_path)
+        self.img = py.transform.scale(self.img, (board.tile_width - 20, board.tile_height - 20))
+        self.notation = 'B'
 
 
-    # TODO get_possible_moves(self, board):
+    def get_possible_moves(self, board):
+        directions = [
+            (1, -1),  # Northeast
+            (1, 1),   # Southeast
+            (-1, 1),  # Southwest
+            (-1, -1)  # Northwest
+        ]
+
+        output = []
+        for dx, dy in directions:
+            path = []
+            x, y = self.x, self.y
+            while True:
+                x += dx
+                y += dy
+                if not (0 <= x < 8 and 0 <= y < 8):
+                    break
+                path.append(board.get_square_from_pos((x,y)))
+            output.append(path)
+        return output
